ea0132ad77a7c713ca9a7cc9888314a90532ffb3
vsedov <viv.sedov@hotmail.com>
2020-12-08 16:58:07 +0000

    Added DeepSource and test folder + Numba and some other stuff

---
+++ tests/test.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020-12-08 Viv Sedov <viv.sedov@hotmail.com>
+#
+# File Name: test.py  Host: viv
+#
+# Distributed under terms of the MIT license.
+
+import better_exceptions
+import numpy as np
+from frosch import hook
+from pprintpp import pprint as pp
+
+better_exceptions.hook()
+hook()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
