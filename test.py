d8f1e2fa378741468590b7d16e4f5bb796507d7a
vsedov <viv.sedov@hotmail.com>
2020-12-13 03:39:32 +0000

    Test Folder added

--- test.py
+++
@@ -1,6 +0,0 @@
-from pprintpp import pprint as pp
-print("for the variable that we have right now ")
-
-
-
-pp("test")
---
+++ tests/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020-12-13 Viv Sedov
+#
+# File Name: test.py
+# Distributed under terms of the MIT license.
+__author__ = "Viv Sedov"
+__email__ = "viv.sb@hotmail.com"
+
+import better_exceptions
+from frosch import hook
+
+better_exceptions.hook()
+hook()
+
+
+def main() -> None:
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
