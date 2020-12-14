42899537683b0048943d13715d896a3008e39d13
vsedov <viv.sedov@hotmail.com>
2020-12-14 05:28:54 +0000

    Created test symlink

--- tests/test.py
+++ tests/test.py
@@ -2,27 +2,22 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 #
-# Copyright © 2020-12-13 Viv Sedov
+# Copyright © 2020-12-14 Viv Sedov 
 #
-# File Name: test.py
+# File Name: test.py  
 # Distributed under terms of the MIT license.
 __author__ = "Viv Sedov"
 __email__ = "viv.sb@hotmail.com"
 
 import better_exceptions
-import numpy as np
 from frosch import hook
 
 better_exceptions.hook()
 hook()
 
 
-def FirstNeuron() -> np:
-    pass
-
-
 def main() -> None:
-
+    
     pass
 
 
