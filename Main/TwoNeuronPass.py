9ce5fc3accca714d6d33bd72443ef33934d046bf
vsedov <viv.sedov@hotmail.com>
2020-12-14 05:20:57 +0000

    Updated req  + Second neuron added

---
+++ Main/TwoNeuronPass.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020-12-13 Viv Sedov
+#
+# File Name: FirstNeuron.py
+# Distributed under terms of the MIT license.
+__author__ = "Viv Sedov"
+__email__ = "viv.sb@hotmail.com"
+
+import better_exceptions
+from frosch import hook
+from pprintpp import pprint as pp
+
+better_exceptions.hook()
+hook()
+
+
+def firstNeuron() -> int:
+    # MultiLyaer perceptron model :  For the first level
+    # Words can be long .
+    # Every neuron has a unique connection to a previous connection .
+    # The output
+    inputs = [1, 2, 3, 2.5]  # Unique inputs and outputs from previous layer
+    weights = [[0.2, 0.8, -0.5, 1], [0.4, 0.3, 0.1, 10]]
+
+    biases = 2  # is the ammount that we have -
+
+    output = (
+        inputs[0] * weights[0]
+        + inputs[1] * weights[1]
+        + inputs[2] * weights[2]
+        + inputs[3] * weights[3]
+        + biases
+    )
+    pp(output)
+    # this would be the first output for first neuron that we made .
+
+    # For the most part
+
+
+def main() -> None:
+
+    firstNeuron()
+
+
+if __name__ == "__main__":
+    main()
+
+    """
+    we break down every step :
+    input = Inputs for the user .
+    weights, in this case we have some sort of line so this output will be the
+    input for the next layer
+    bias is the value that we have atm .
+    """
