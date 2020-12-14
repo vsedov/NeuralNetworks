c30015eb38e42abb17b46bb74eb1876b3afa16b1
vsedov <viv.sedov@hotmail.com>
2020-12-14 02:48:28 +0000

    4 Neurons, test, more infomation on visulizer

--- Main/FirstNeuron.py
+++ Main/FirstNeuron.py
@@ -12,6 +12,7 @@ __email__ = "viv.sb@hotmail.com"
 import better_exceptions
 import numpy as np
 from frosch import hook
+from pprintpp import pprint as pp
 
 better_exceptions.hook()
 hook()
@@ -22,18 +23,19 @@ def firstNeuron() -> np:
     # Words can be long .
     # Every neuron has a unique connection to a previous connection .
     # The output
-    inputs = [1.2, 2.5, 2.1]  # Unique inputs and outputs from previous layer
-    weights = [3.1, 2.1, 8.9]  # Each input, and a weight for each input
-    biases = 3  # is the ammount that we have -
+    inputs = [1, 2, 3, 2.5]  # Unique inputs and outputs from previous layer
+    weights = [0.2, 0.8, -0.5, 1]  # Each input, and a weight for each input
+    biases = 2  # is the ammount that we have -
 
     output = (
         inputs[0] * weights[0]
         + inputs[1] * weights[1]
         + inputs[2] * weights[2]
+        + inputs[3] * weights[3]
         + biases
     )
-    print(output)  # In this case
-
+    pp(output)
+    pp(np.dot(inputs, weights) + 2)
     # this would be the first output for first neuron that we made .
 
     # For the most part
