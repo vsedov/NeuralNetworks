fdf1f635269623c1997d0ba369eb1126f626935b
vsedov <viv.sedov@hotmail.com>
2020-12-20 02:20:46 +0000

    Batches, and layers - Further infomation added later

--- Main/batchAndLayers.py
+++ Main/batchAndLayers.py
@@ -15,7 +15,7 @@ from pprintpp import pprint as pp
 
 def calCu() -> np:  # Shape Error
 
-    inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+    inputs = [1, 2, 3, 2.5]
 
     weights = [
         [0.2, 0.8, -0.5, 1.0],
@@ -35,7 +35,7 @@ def calCuFixShape() -> np:  # Shape Error
 
     weights = [
         [0.2, 0.8, -0.5, 1.0],
-        [0.5, 0.8, 0.26, -0.5],
+        [0.5, -0.91, 0.26, -0.5],
         [-0.26, -0.27, 0.17, 0.87],
     ]
 
@@ -50,6 +50,69 @@ def calCuFixShape() -> np:  # Shape Error
     output = np.dot(inputs, np.array(weights).T) + bias
 
     pp(output)
+    print(len(inputs), len(weights), sep="\n")
+
+    print("Going to the second layer \n")
+
+    # In this case, the given layer, : has to be the same as the amount of bias and weights
+    # we had before
+
+    weights2 = [
+        [0.1, -0.14, 0.5],
+        [-0.5, 0.12, -0.33],
+        [-0.44, 0.73, -0.13],
+    ]
+
+    bias2 = [-1, 2, -0.5]  # We are not changing anything else here
+
+    layer1_Output = np.dot(inputs, np.array(weights).T) + bias
+
+    # Layer1_Ouput = becomes into layer1 inputs basiclly if that makes sense
+
+    layer2_Output = np.dot(layer1_Output, np.array(weights2).T) + bias2
+
+    pp(layer2_Output)
+
+    # This can become rather long
+
+
+def classer():
+    X = [
+        [0.2, 0.8, -0.5, 1.0],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
+    ]  # Input data is always an X
+
+    # Defining the two latent space
+
+    # You can normalise this data as well and you can scale it -1 and +1
+
+    np.random.seed(0)
+
+    layer1 = Layer_Dense(4, 5)  # This is number of neurons .
+    # the input has to be the same as the layer value
+    layer2 = Layer_Dense(5, 6)
+
+    layer1.forward(X)
+    # print(layer1.outpt)
+
+    layer2.forward(layer1.output)
+    print(layer2.output)
+
+
+class Layer_Dense:  # making a new neural networks := Weights First -1<= x <=1
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(
+            n_inputs, n_neurons
+        )  # we need to know the shape Size fo input and How many Neurons
+        # Size of a single sample so 4 ^
+        self.biases = np.zeros(
+            (1, n_neurons)
+        )  # will be 1 by how many neurons that you have - Needs to be a tuple of those two
+
+    def forward(self, inputs):  # This could be the training data
+
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
 def main() -> None:
@@ -57,6 +120,8 @@ def main() -> None:
     # print("\n")
 
     calCuFixShape()
+    print("Changing this into a class \n")
+    classer()
 
 
 if __name__ == "__main__":
