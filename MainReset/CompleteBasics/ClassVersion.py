f6d37d938fc0dfcccbab573becb9dcd368caab0d
vsedov <viv.sedov@hotmail.com>
2021-07-02 03:58:15 +0100

    Clean up

--- MainReset/CompleteBasics/ClassVersion.py
+++ MainReset/CompleteBasics/ClassVersion.py
@@ -245,18 +245,13 @@ def main() -> None:
 
 def main_version_2() -> None:
     # Create dataset
-    X, y = spiral_data(samples=100, classes=3)
+    X, y = spiral_data(samples=100, classes=3)  # noqa :ignore
 
-    # Create Dense layer with 2 input features and 64 output values
     dense1 = LayerDense(2, 64)
 
-    # Create ReLU activation (to be used with Dense layer):
     activation1 = ActivationRelu()
 
-    # Create second Dense layer with 64 input features (as we take output
-    # of previous layer here) and 3 output values (output values)
     dense2 = LayerDense(64, 3)
-    # Create Softmax classifier's combined loss and activation
     loss_activation = ActivationSoftMaxCCELoss()
 
     # Create optimizer
@@ -265,28 +260,15 @@ def main_version_2() -> None:
     # Train in loop
     for epoch in range(10001):
 
-        # Perform a forward pass of our training data through this layer
         dense1.forward(X)
 
-        # Perform a forward pass through activation function
-        # takes the output of first dense layer here
         activation1.forward(dense1.output)
 
-        # Perform a forward pass through second Dense layer
-        # takes outputs of activation function of first layer as inputs
         dense2.forward(activation1.output)
 
-        # Perform a forward pass through the activation/loss function
-        # takes the output of second dense layer here and returns loss
         loss = loss_activation.forward(dense2.output, y)
 
-        # Calculate accuracy from output of activation2 and targets
-        # calculate values along first axis
-        predictions = np.argmax(loss_activation.output, axis=1)
-        if len(y.shape) == 2:
-            y = np.argmax(y, axis=1)
-        accuracy = np.mean(predictions == y)
-
+        accuracy = loss_activation.accuracy(loss_activation.output, y)
         if not epoch % 100:
             print(
                 f"epoch: {epoch}, " + f"acc: {accuracy:.3f}, " + f"loss: {loss:.3f}, "
